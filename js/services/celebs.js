bb56160e5b9c816b3fadb856c12811889bd33212
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-10-18 00:09:09 -0400

    changed path to JSON file

--- js/services/celebs.js
+++ js/services/celebs.js
@@ -6,13 +6,13 @@ angular.module('Celebs')
         getAll: function () {
             return $http({
                 method: 'GET',
-                url: '../../data/celebs.json'
+                url: '/data/celebs.json'
             })
         },
         getOne: function (id) {
             return $http({
                 method: 'GET',
-                url: '../../data/celebs.json'
+                url: '/data/celebs.json'
             })
         }
     }
